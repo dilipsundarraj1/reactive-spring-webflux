4eb25b772acb2e9c16b8c270f66b7ae6fbafcc20
Dilip Sundarraj <you@example.com>
2021-08-05 05:57:23 -0500

    added the inital commit for movies review service

---
+++ movies-review-service/src/main/java/com/reactivespring/MoviesReviewServiceApplication.java
@@ -0,0 +1,13 @@
+package com.reactivespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesReviewServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesReviewServiceApplication.class, args);
+	}
+
+}
---
+++ movies-review-service/src/test/java/com/reactivespring/MoviesReviewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactivespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesReviewServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
