16f832baf1f368339b2b2e4d7f3624e8ee50f0f3
Dilip Sundarraj <you@example.com>
2021-08-10 05:41:34 -0500

    completed the intial project for movies service

---
+++ movies-service/src/main/java/com/reactivespring/MoviesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.reactivespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesServiceApplication.class, args);
+	}
+
+}
---
+++ movies-service/src/test/java/com/reactivespring/MoviesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactivespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
