ab7c1ee9ab350eacf62d0ce7812dadd1837d5541
dilipsundarraj <dilip.bandlasundarraj@gmail.com>
2021-08-17 04:45:35 -0500

    fixing the FluxandMonoControllerunittest

--- movies-info-service/src/test/java/unit/com/reactivespring/controller/FluxMonoControllerUnitTest.java
+++ movies-info-service/src/test/java/unit/com/reactivespring/controller/FluxMonoControllerUnitTest.java
@@ -1,20 +1,25 @@
 package com.reactivespring.controller;
 
+import com.reactivespring.service.MoviesInfoService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.test.StepVerifier;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@ActiveProfiles("test")
+@WebFluxTest(controllers = FluxMonoController.class)
 public class FluxMonoControllerUnitTest {
 
     @Autowired
     WebTestClient webTestClient;
+
+    @MockBean
+    private MoviesInfoService moviesInfoServiceMock;
 
     @Test
     void flux() {
