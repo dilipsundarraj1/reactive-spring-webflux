d74043b020a288aad00a391d9df218be8b44e411
dilipsundarraj <dilip.bandlasundarraj@gmail.com>
2021-09-04 04:39:11 -0500

    added sinks

--- movies-info-service/src/main/java/com/reactivespring/controller/MoviesInfoController.java
+++ movies-info-service/src/main/java/com/reactivespring/controller/MoviesInfoController.java
@@ -7,11 +7,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.ConnectableFlux;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.publisher.Sinks;
 
+import javax.annotation.PostConstruct;
 import javax.print.attribute.standard.Media;
 import javax.validation.Valid;
 
